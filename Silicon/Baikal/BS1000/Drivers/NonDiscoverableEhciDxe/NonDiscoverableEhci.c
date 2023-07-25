7e71c1f3a099cbdcc91bb7a9622fe39030af03a9
Vadim V. Vlasov <vadim.vlasov@elpitech.ru>
2023-07-25 19:34:22 +0300

    BS1000: Add Ehci hub reset and configuration

--- Silicon/Baikal/BS1000/Drivers/NonDiscoverableEhciDxe/NonDiscoverableEhci.c
+++ Silicon/Baikal/BS1000/Drivers/NonDiscoverableEhciDxe/NonDiscoverableEhci.c
@@ -5,9 +5,30 @@
 
 #include <PiDxe.h>
 #include <Library/DebugLib.h>
+#include <Library/DwI2cLib.h>
+#include <Library/GpioLib.h>
 #include <Library/NonDiscoverableDeviceRegistrationLib.h>
+#include <Library/UefiBootServicesTableLib.h>
 
 #include <BS1000.h>
+
+#ifdef ELPITECH
+STATIC CONST UINT8 UsbHubCfg0[] = {
+  0, /* start offset */
+  17, /* size of data */
+  0x24, 0x04, 0x17, 0x25,
+  0x00, 0x00, 0x9b, 0x20,
+  0x00, 0x00, 0x00, 0x00,
+  0x32, 0x32, 0x32, 0x32,
+  0x32
+};
+
+STATIC CONST UINT8 UsbHubCfgff[] = {
+  0xff, /* offset */
+  1, /* size */
+  0x01 /* enable */
+};
+#endif
 
 EFI_STATUS
 EFIAPI
@@ -17,6 +38,30 @@ NonDiscoverableEhciEntryPoint (
   )
 {
   EFI_STATUS  Status;
+
+#ifdef ELPITECH
+  GpioOutRst(BS1000_GPIO32_BASE, 6);
+  GpioDirSet(BS1000_GPIO32_BASE, 6);
+  gBS->Stall(20);
+  GpioOutSet(BS1000_GPIO32_BASE, 6);
+  gBS->Stall(500);
+  INTN Ret;
+  Ret = I2cTxRx (BS1000_I2C6_BASE,
+           100000000,
+           0x2c,
+           UsbHubCfg0,
+           sizeof(UsbHubCfg0),
+           0, 0);
+  if (Ret < 0) {
+    DEBUG((EFI_D_ERROR, "USB hub config: ret %d\n", Ret));
+  }
+  I2cTxRx (BS1000_I2C6_BASE,
+           100000000,
+           0x2c,
+           UsbHubCfgff,
+           sizeof(UsbHubCfgff),
+           0, 0);
+#endif
 
   Status = RegisterNonDiscoverableMmioDevice (
              NonDiscoverableDeviceTypeEhci,
